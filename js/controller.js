534f58a81e2884c029a8dbb57122cfe615363e1c
Morris Buel <morris.buel@gmail.com>
2021-04-14 22:02:15 -0700

    completed todo react tutorial

--- js/controller.js
+++ js/controller.js
@@ -1,63 +1,13 @@
-var url = 'https://altcademy-to-do-list-api.herokuapp.com/tasks';
-// var query = '?';
-var apiKey = '?api_key=13'
+var project = 'React Todo';
+var copyrightYear = new Date().getFullYear();
 
-xhr('GET', url + apiKey, createList);
+var init = function init() {
+  $('#project').innerText = project;
+  $('#copyright-year').innerText = copyrightYear;
 
-// TODO: change to jQUERY call for document ready
+  ReactDOM.render(React.createElement(ToDoList, null), document.getElementById('root'));
+};
 
-// TODO: bonus better define MVC functions on appropriate fornm
-
-$(document).ready(function () {
-
-
-    $('#todo-list').on('click', 'div.col-1>input', function(event) {
-      var id = event.currentTarget.parentNode.parentNode.id.split('-')[1];
-      var completed = event.currentTarget.checked;
-
-      setCompleted(id, setCompletedCheckBox, completed);
-
-    });
-
-    $('#todo-list').on('click', 'div.col-9>input', function(event) {
-      event.preventDefault();
-
-      var id = event.currentTarget.parentNode.parentNode.id.split('-')[1];
-
-      displayElement(`#update-${id}`);
-    });
-
-    // deal with button click per row
-    $('#todo-list').on('click', 'button', function(event) {
-      event.preventDefault();
-      var type = event.currentTarget.classList[event.currentTarget.classList.length - 1].split('-')[0];
-      var id = event.currentTarget.parentNode.parentNode.id.split('-')[1];
-      switch(type) {
-        case 'update':
-          // send update to server
-          updateTodo(id, setCompletedCheckBox);
-          break;
-        case 'remove':
-          deleteTodo(id);
-
-          break;
-      }
-    })
-    
-    $('.btn-group').on('click', function() {
-      setTimeout(function() {
-        var filter = $('.btn-group').find('.active')[0].children[0].id;
-        console.log(filter);
-        filterList(filter);
-
-      },50);
-    });
-
-    // This handler will execute when the addButton is clicked.
-    $('div#input-form > div.row > div').on('click', '.btn', checkThenAddTodo);
-    $('document').on('keyup', function (event) {
-        if (event.key === "Enter") {
-          checkThenAddTodo();
-        }
-    });
+$('document').ready(function () {
+  init();
 });
